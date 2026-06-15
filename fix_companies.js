6bfa97d509a6a3dbd7ac3a77a06b516a1530a4ed
LAKSHMINARASIMHATM <narasimha01tm@gmail.com>
2026-06-15 13:12:15 +0530

    fixed all pages

--- fix_companies.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let text = fs.readFileSync('backend/data/companies.js', 'utf8');
-text = text.replace(/},\r?\n,\r?\n/g, '},\n');
-fs.writeFileSync('backend/data/companies.js', text);
-console.log('Fixed double comma.');
